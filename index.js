64340c2366440a1ec33bdacd86fe3b5cfe62a43f
NicholasLeao <tropicaldata@gmail.com>
2023-11-07 11:20:17 -0300

    finished api, will refactor for more egonomics

--- index.js
+++ index.js
@@ -12,14 +12,25 @@
 
 const animationConfig = {
   "multiple-staggered-fadein": {
-    child_class: "animated-child",
+    child_class: "hidden-animated",
     parent_styles: "backgroundColor:#7d7d7d;padding:50px;",
-    child_styles: "opacity:1;",
-    stagger: 300,
+    child_styles: `
+      opacity:1;
+    `,
+    stagger: 100,
+  },
+  "multiple-staggered-pop": {
+    child_class: "pop-animated",
+    parent_styles: "backgroundColor:#7d7d7d;padding:50px;",
+    child_styles: `
+      backgroundColor:#606c38;
+      boxShadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+    `,
+    stagger: 500,
   },
 };
 
-// Movement direction constants
+// Movement direction enum
 const directions = {
   UP_ENTER: "up_enter",
   UP_LEAVE: "up_leave",
@@ -31,6 +42,7 @@ const directions = {
 const INTERSECTION_OBSERVER_SUPPORT = "IntersectionObserver" in window;
 
 const applyStyles = (el, styles) => {
+  // Based on styles string, applies parsed styles to element.
   styles
     .trim()
     .split(";")
@@ -41,25 +53,58 @@ const applyStyles = (el, styles) => {
     });
 };
 
+const removeStyles = (el, styles) => {
+  // Based on styles string, removes parsed styles to element.
+  styles
+    .trim()
+    .split(";")
+    .forEach((style) => {
+      if (style === "") return;
+      const [k, _] = style.split(":");
+      el.style[k?.trim()] = "";
+    });
+};
+
 const setDataAnimated = (el, val) => {
   // Sets data-animated attribute
   el.setAttribute("data-animated", val);
+};
+
+const removeDataAnimation = (el) => {
+  // Funtion is analogous to applyData animation but removes styles.
+  const attr = el.getAttribute("data-animation");
+  const config = animationConfig[attr];
+  if (!config) return;
+  if (config.parent_styles) removeStyles(el, config.parent_styles);
+  el.querySelectorAll(`.${config.child_class}`).forEach((node, _) => {
+    if (config?.stagger) {
+      node.style.transitionDelay = "";
+    }
+    if (config?.child_styles) {
+      removeStyles(node, config.child_styles);
+    }
+  });
 };
 
 const applyDataAnimation = (el) => {
   // Retrieve config with data attribute
   const attr = el.getAttribute("data-animation");
 
+  // Find configuration for specific attribute
   const config = animationConfig[attr];
   if (!config) return;
 
+  // Apply parent styles if present
   if (config.parent_styles) applyStyles(el, config.parent_styles);
 
+  // Find child elements based on child_class
   el.querySelectorAll(`.${config.child_class}`).forEach((node, idx) => {
+    // If stagger, apply transition delay
     if (config?.stagger) {
       node.style.transitionDelay = `${idx * config.stagger}ms`;
     }
 
+    // If child_styles, apply them to child node
     if (config?.child_styles) {
       applyStyles(node, config.child_styles);
     }
@@ -67,38 +112,29 @@ const applyDataAnimation = (el) => {
 };
 
 const entryDirection = (entry, state) => {
-  /** Function uses previous rect data to derive direction and if entering or leaving */
   const currY = entry.boundingClientRect.y;
   const currRatio = entry.intersectionRatio;
   const isIntersecting = entry.isIntersecting;
 
-  // Current Y < Previous Y indicates downwards motion of observed el
+  // Based on current Y and stored Y, find motion direction.
+  //  Then find if element entering or existing based on intersectionRatio.
   if (currY < state.prevY) {
-    // Current Ratio > Previous Ratio indicates element is entering viewport
-    // Is intersecting indicates element is on screen and therefore not leaving
-    if (currRatio > state.prevRatio && isIntersecting) {
-      return directions.DOWN_ENTER;
-    } else {
-      return directions.DOWN_LEAVE;
-    }
-    // Current Y > Previous Y indicates upwards motion of observed el
-  } else if (currY > state.prevY && isIntersecting) {
-    // Current Ratio < Previous Ratio indicates element is leaving viewport
-    if (currRatio < state.prevRatio) {
-      return directions.UP_LEAVE;
-    } else {
-      return directions.UP_ENTER;
-    }
+    return currRatio > state.prevRatio && isIntersecting
+      ? directions.DOWN_ENTER
+      : directions.DOWN_LEAVE;
+  } else if (currY > state.prevY) {
+    return currRatio > state.prevRatio && isIntersecting
+      ? directions.UP_ENTER
+      : directions.UP_LEAVE;
   }
 };
 
 const initIntersectionObserver = () => {
-  document.querySelectorAll('[data-animated="false"]')?.forEach((el) => {
+  document.querySelectorAll('[data-animated="true"]')?.forEach((el) => {
     // Clause checks if element is already above current view
     const r = el.getBoundingClientRect();
     if ((r.top >= 0 && r.bottom <= window.innerHeight) || r.top < 0) {
       applyDataAnimation(el);
-      setDataAnimated(el, true);
     }
 
     // Form closure over object to hold previous state data
@@ -107,15 +143,13 @@ const initIntersectionObserver = () => {
       prevRatio: 0,
     };
 
+    // Intersection observer API callback
     const fn = (entries) => {
       const dir = entryDirection(entries[0], prev);
-
-      // Sets data-animated attribute to true if element is entering and false if exiting
       if (dir === directions.UP_ENTER || dir === directions.DOWN_ENTER) {
         applyDataAnimation(el);
-        setDataAnimated(el, true);
       } else {
-        setDataAnimated(el, false);
+        removeDataAnimation(el);
       }
     };
 
@@ -128,7 +162,7 @@ const initIntersectionObserverFallback = () => {
   // Add scroll event listener
   window.addEventListener("scroll", () => {
     // Retrieve all elemetns with data-animated = false
-    document.querySelectorAll('[data-animated="false"]').forEach((el) => {
+    document.querySelectorAll('[data-animated="true"]').forEach((el) => {
       // Clause checks if element is already above current view
       const r = el.getBoundingClientRect();
       if ((r.top >= 0 && r.bottom <= window.innerHeight) || r.top < 0) {
